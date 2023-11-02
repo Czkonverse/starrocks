c7ac0fb9da490c8db8009f9799cd43cac4fc923a
yiming <107105845+nshangyiming@users.noreply.github.com>
2023-11-02 10:28:10 +0800

    [Enhancement] Add more log info when ingestion failure caused by lost replica (#33856)
    Add more info in log to tell whether the ingestion failure is caused by
    dead/decommissioned backend or abnormal state replica.
    
    Signed-off-by: Dejun Xia <xiadejun@starrocks.com>
    (cherry picked from commit a69ce0d195ac1babc301b420b40cad4e49c0399f)

--- fe/fe-core/src/main/java/com/starrocks/service/FrontendServiceImpl.java
+++ fe/fe-core/src/main/java/com/starrocks/service/FrontendServiceImpl.java
@@ -34,7 +34,6 @@
 
 package com.starrocks.service;
 
-import com.google.common.base.Joiner;
 import com.google.common.base.Preconditions;
 import com.google.common.base.Strings;
 import com.google.common.collect.Lists;
@@ -2177,7 +2176,7 @@ public class FrontendServiceImpl implements FrontendService.Iface {
                     if (bePathsMap.keySet().size() < quorum) {
                         throw new UserException(
                                 "Tablet lost replicas. Check if any backend is down or not. tablet_id: "
-                                        + tablet.getId() + ", backends: " + Joiner.on(",").join(localTablet.getBackends()));
+                                        + tablet.getId() + ", replicas: " + localTablet.getReplicaInfos());
                     }
                     // replicas[0] will be the primary replica
                     // getNormalReplicaBackendPathMap returns a linkedHashMap, it's keysets is stable
@@ -2308,7 +2307,7 @@ public class FrontendServiceImpl implements FrontendService.Iface {
                         } catch (UserException exception) {
                             errorStatus.setError_msgs(Lists.newArrayList(
                                     "Tablet lost replicas. Check if any backend is down or not. tablet_id: "
-                                            + tablet.getId() + ", backends: none"));
+                                            + tablet.getId() + ", backends: none(cloud native table)"));
                             result.setStatus(errorStatus);
                             return result;
                         }
@@ -2323,8 +2322,7 @@ public class FrontendServiceImpl implements FrontendService.Iface {
                         if (bePathsMap.keySet().size() < quorum) {
                             errorStatus.setError_msgs(Lists.newArrayList(
                                     "Tablet lost replicas. Check if any backend is down or not. tablet_id: "
-                                            + tablet.getId() + ", backends: " +
-                                            Joiner.on(",").join(localTablet.getBackends())));
+                                            + tablet.getId() + ", replicas: " + localTablet.getReplicaInfos()));
                             result.setStatus(errorStatus);
                             return result;
                         }
